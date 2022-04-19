34f411d9ae3c00804a4d90dcc2e59e57680f05f2
Norbert <szilvasi.norbert@gmail.com>
2022-04-19 05:31:41 +0200

    modification

--- src/hu/Models/Author.java
+++ src/hu/Models/Author.java
@@ -1,4 +1,36 @@
 package hu.Models;
 
 public class Author {
+
+    private String name;
+    private String country;
+    private int birthYear;
+
+    //public Author(String ){}
+
+    public Author(String name, String country, int birthYear) {
+        this.name = name;
+        this.country = country;
+        this.birthYear = birthYear;
+    }
+
+    public String toString() {
+        return name + " " + country + " " + birthYear;
+    }
+
+    public static void main(String[] args) {
+        Author author1 = new Author("Mario Puzo", "USA", 1920);
+        System.out.println("Print");
+        printAuthor(author1);
+
+    }
+
+    public static void printAuthor(Author author) {
+        System.out.println("\n\t");
+        System.out.println("Information:");
+
+        System.out.println("\n\t" + "author : " + author.name + "\n\t" +
+                "country :" + author.country + "\n\t" +
+                "birthYear :" + author.birthYear);
+    }
 }
